5c33ed9bee322a89579488daa6b9322e56e0a316
sawee4 <c44.leblois@gmail.com>
2025-06-08 18:18:21 +0200

    Update botia.py

--- botia.py
+++ botia.py
@@ -1,2 +1,14 @@
-print("hello world")
-print("hello world2")
+
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import language_tool_python
+
+app = FastAPI()
+tool = language_tool_python.LanguageTool('fr')# Pour le français
+
+class EmailText(BaseModel):
+    texte: str
+
+@app.post("/analyse")
+async def analyse_email(data: EmailText):
+    original = data.texte
